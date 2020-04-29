b7b453ed4e61c3f37617ba77ad6fd9f6c919e7be
Kristof S <kristof.attila.private@gmail.com>
2020-04-29 21:05:25 +0200

    vectorization and acceleration updated

--- lhsmdu_mod.py
+++ lhsmdu_mod.py
@@ -12,34 +12,27 @@
 #Author:Kristof S.
 #modified functions : #ModKS
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-from numpy import random, matrix, zeros, triu_indices, sum, argsort, ravel, max
-from numpy import min as minimum
-from scipy.stats import rv_continuous, rv_discrete
-from scipy.stats.distributions import rv_frozen
-from numba import jit
-import customWraps as cw
+from   __future__ import absolute_import, division, print_function, unicode_literals
+import numpy 
+import scipy
+import   numba 
 
 ##### Default variables #####
-scalingFactor = 5 ## number > 1 (M) Chosen as 5 as suggested by the paper (above this no improvement.
-numToAverage = 2 ## Number of nearest neighbours to average, as more does not seem to add more information (from paper).
-randSeed = 42 ## Seed for the random number generator 
-random.seed(randSeed) ## Seeding the random number generator.
+scalingFactor   = 5 ## number > 1 (M) Chosen as 5 as suggested by the paper (above this no improvement.
+numToAverage    = 2 ## Number of nearest neighbours to average, as more does not seem to add more information (from paper).
+randSeed        = 42 ## Seed for the random number generator 
+numpy.random.seed(randSeed) ## Seeding the random number generator.
 
 def setRandomSeed(newRandSeed):
     global randSeed
     randSeed = newRandSeed
-    random.seed(randSeed) ## Seeding the random number generator.
-
+    numpy.random.seed(randSeed) ## Seeding the random number generator.
 
 def createRandomStandardUniformMatrix(nrow, ncol): 
     ''' Creates a matrix with elements drawn from a uniform distribution in [0,1]'''
-    #ModKS #accelerated with numpy (25* faster)
-    #rows = matrix([ [random.random() for i in range(ncol)] for j in range(nrow)])
-    return numpy.random.uniform(size=(nrow,ncol))
+    return numpy.random.uniform(size=(nrow,ncol))     #ModKS #usage of numpy (25* faster)
 
-
-@jit(nopython=True, parallel = True)
+@numba.jit(nopython=True, parallel = True)
 def findUpperTriangularColumnDistanceVector(inputMatrix):
     ''' Finds the 1-D upper triangular euclidean distance vector for the columns of a matrix.'''
     #ModKS #assert ncol == inputMatrix.shape[1] => just say that ncol = inputMatrix.shape[1] and remove arg from function
@@ -55,38 +48,47 @@ def findUpperTriangularColumnDistanceVector(inputMatrix):
 #@jit(nopython=True)
 def createSymmetricDistanceMatrix(distance, nrow):
     ''' Creates a symmetric distance matrix from an upper triangular 1D distance vector.'''
-    distMatrix = zeros((nrow,nrow))
-    indices = triu_indices(nrow,k=1)
-    distMatrix[indices] = distance
+    # Fast enough
+    distMatrix                           = numpy.zeros((nrow,nrow))
+    indices                              = numpy.triu_indices(nrow,k=1)
+    distMatrix[indices]                  = distance
     distMatrix[(indices[1], indices[0])] = distance # Making symmetric matrix
     return distMatrix
 
-#@jit(nopython=True)
+
 def eliminateRealizationsToStrata(distance_1D, matrixOfRealizations, numSamples, numToAverage = numToAverage):
     ''' Eliminating realizations using average distance measure to give Strata '''
 
-    numDimensions = matrixOfRealizations.shape[0]
+    numDimensions   = matrixOfRealizations.shape[0]
     numRealizations = matrixOfRealizations.shape[1]
     ## Creating a symmetric IxI distance matrix from the triangular matrix 1D vector.
-    distMatrix = createSymmetricDistanceMatrix(distance_1D, numRealizations)
+    distMatrix      = createSymmetricDistanceMatrix(distance_1D, numRealizations)
  
     ## Finding columns from the realization matrix by elimination of nearest neighbours L strata are left.
-    averageDistance = {i:0 for i in range(numRealizations)}
-    
-    while(len(averageDistance)>numSamples):
-        for rowNum in sorted(averageDistance.keys()):
-            meanAvgDist = sum( sorted( distMatrix[ rowNum, sorted(averageDistance.keys())])[:numToAverage+1])/numToAverage
-            averageDistance.update( {rowNum: meanAvgDist }) # +1 to remove the zero index, appending averageDistance to list
-        indexToDelete = min(averageDistance, key=averageDistance.get)
-        del averageDistance[indexToDelete]
-    
+    ## Using matrices is faster, and works with numba
+    averageDistanceIdx  = list(numpy.arange(numRealizations).astype(int))
+    averageDistanceData = list(numpy.zeros(numRealizations))
+    ## using external function for numba
+    averageDistanceIdx = fasterSorting(averageDistanceIdx, averageDistanceData, distMatrix, numSamples, numToAverage)
     # Creating the strata matrix to draw samples from.
-    StrataMatrix = matrixOfRealizations[:,sorted(averageDistance.keys())]
-
-    assert numSamples == StrataMatrix.shape[1]
+    StrataMatrix = matrixOfRealizations[:,averageDistanceIdx]
+    
+    assert numSamples    == StrataMatrix.shape[1]
     assert numDimensions == StrataMatrix.shape[0]
     #print ( StrataMatrix )
     return StrataMatrix
+
+def fasterSorting(averageDistanceIdx, averageDistanceData, distMatrix, numSamples, numToAverage):
+    var = len(averageDistanceIdx)
+    while(var>numSamples):
+        for rowNum in averageDistanceIdx:
+            meanAvgDist = numpy.divide(numpy.sum(numpy.sort(distMatrix[rowNum, averageDistanceIdx])[:numToAverage+1]), numToAverage)
+            averageDistanceData[averageDistanceIdx.index(rowNum)] = meanAvgDist  # +1 to remove the zero index, appending averageDistance to list
+        indexToDelete = averageDistanceData.index(min(averageDistanceData))
+        del(averageDistanceIdx[indexToDelete]) 
+        del(averageDistanceData[indexToDelete])
+        var = len(averageDistanceIdx)
+    return averageDistanceIdx
 
 #@jit(nopython=True)
 def inverseTransformSample(distribution, uniformSamples):
@@ -94,31 +96,27 @@ def inverseTransformSample(distribution, uniformSamples):
     a sample from an arbitrary distribution. This is done by taking the cdf [0,1] of 
     the arbitrary distribution, and calculating its inverse to picking the sample."
     '''
-    assert (isinstance(distribution, rv_continuous) or isinstance(distribution, rv_discrete) or isinstance(distribution,rv_frozen))
+    assert (isinstance(distribution, scipy.stats.rv_continuous) or isinstance(distribution, scipy.stats.rv_discrete) or isinstance(distribution,scipy.stats.distributions.rv_frozen))
     newSamples = distribution.ppf(uniformSamples)
     return newSamples
     
-#@jit(nopython=True) 
-def resample():
+@numba.jit(nogil=True) 
+def resample(matrixOfStrata):
 
     ''' Resampling function from the same strata'''
-    numDimensions = matrixOfStrata.shape[0]
-    numSamples = matrixOfStrata.shape[1]
-
-    matrixOfSamples = []
-    
+    numDimensions   = matrixOfStrata.shape[0]
+    numSamples      = matrixOfStrata.shape[1]
+    matrixOfSamples = numpy.empty((numDimensions, numSamples))
     # Creating Matrix of Samples from the strata ordering.
     for row in range(numDimensions):
-        sortedIndicesOfStrata = argsort(ravel(matrixOfStrata[row,:]))
-    
+        sortedIndicesOfStrata = numpy.argsort(numpy.ravel(matrixOfStrata[row,:]))
         # Generating stratified samples
-        newSamples =  [ (float(x)/numSamples) + (random.random()/numSamples) for x in sortedIndicesOfStrata ]
-        matrixOfSamples.append(newSamples)
-    
-    assert minimum(matrixOfSamples)>=0.
-    assert max(matrixOfSamples)<=1.
-    
-    return matrix(matrixOfSamples)
+        newSamples =  [ (float(x)/numSamples) + (numpy.random.random()/numSamples) for x in sortedIndicesOfStrata ]
+        matrixOfSamples[:,row]=newSamples
+
+    #assert numpy.min(matrixOfSamples)>=0.
+    #assert numpy.max(matrixOfSamples)<=1.
+    return matrixOfSamples
 
 #@jit(nopython=True)
 def sample(numDimensions, numSamples, scalingFactor=scalingFactor, numToAverage = numToAverage, randomSeed=randSeed):
@@ -128,20 +126,17 @@ def sample(numDimensions, numSamples, scalingFactor=scalingFactor, numToAverage 
         setRandomSeed(randomSeed)
 
     ### Number of realizations (I) = Number of samples(L) x scale for oversampling (M)
-    numRealizations = scalingFactor*numSamples ## Number of realizations (I)
+    numRealizations      = scalingFactor*numSamples ## Number of realizations (I)
     ### Creating NxI realization matrix
-    matrixOfRealizations =  createRandomStandardUniformMatrix(numDimensions, numRealizations)
-    
+    matrixOfRealizations =  createRandomStandardUniformMatrix(numDimensions, numRealizations)  #ModKS
     ### Finding distances between column vectors of the matrix to create a distance matrix.
-    #ModKS 
-    distance_1D = findUpperTriangularColumnDistanceVector(matrixOfRealizations) #, numRealizations
+    distance_1D          = findUpperTriangularColumnDistanceVector(matrixOfRealizations)       #ModKS
     
     ## Eliminating columns from the realization matrix, using the distance measure  to get a strata
     ## matrix with number of columns as number of samples requried.
 
-    global matrixOfStrata
     matrixOfStrata = eliminateRealizationsToStrata(distance_1D, matrixOfRealizations, numSamples)
 
-    matrixOfSamples = resample() 
+    matrixOfSamples = resample(matrixOfStrata) 
     
     return matrixOfSamples
