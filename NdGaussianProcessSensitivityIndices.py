404e0b3a6a8954c44143ce394c5a2401f1b43b79
Kristof S <kristof.attila.private@gmail.com>
2020-05-11 13:53:21 +0200

    some updates

--- NdGaussianProcessSensitivityIndices.py
+++ NdGaussianProcessSensitivityIndices.py
@@ -21,7 +21,54 @@ sensitivity index. This will allow us to scale the method to any type of input s
 
 
 class NdGaussianProcessSensitivityIndicesBase(object):
-	'''Basic methods to calculate unitary sensitivity indices
-	'''
-	@staticmethod
-	def SaltelliIndices(vectA, vectB, vectBAi):
+    '''Basic methods to calculate unitary sensitivity indices
+    We first set the samples Y_A and Y_B and calculate the means and
+    variances of those, so they don't have to be calculated again. 
+    The notations are those of A. DUMAS in the paper :
+    "Lois asymptotiques des estimateurs des indices de Sobol"
+
+
+    This class can accept vectors (unidimensional outputs) as well
+    as matrices (multidimensional outputs)
+    '''
+    def __init__(self):
+
+    def setSamples(Y_A, Y_B):
+        self.Y_A = numpy.squeeze(numpy.array(Y_A))
+        self.Y_B = numpy.squeeze(numpy.array(Y_B))
+        assert self.Y_A.shape == self.Y_B.shape, "samples have to have same shape"
+        self.sampleShape = self.Y_A.shape
+        self.N_samples   = self.sampleShape[0]
+        #Centering of samples (hopefully rightly done)
+        self.Y_Ac = self.Y_A-self.Y_A.mean(axis=0)
+        self.Y_Bc = self.Y_B-self.Y_B.mean(axis=0)
+
+    @staticmethod
+    def checkSampleIntegretyAndCenter(Y_A, Y_B, Y_E):
+        Y_A = numpy.squeeze(Y_A)
+        Y_B = numpy.squeeze(Y_B)
+        Y_E = numpy.squeeze(Y_E)
+        Y_Ac = Y_A  - Y_A.mean(axis=0)
+        Y_Bc = Y_B  - Y_B.mean(axis=0)
+        Y_Ec = Y_E  - Y_E.mean(axis=0)
+        assert Y_A.shape == Y_B.shape == Y_E.shape ,"samples have to have same shape"
+        return Y_Ac, Y_Bc, Y_Ec
+
+    @staticmethod
+    def SaltelliIndices(Y_Ac, Y_Bc, Y_Ec):
+        assert (Y_Ac.shape == Y_Bc.shape == Y_Ec.shape ), "samples have to have same shape"
+        N = Y_Ac.shape[0]
+        Ni = 1/N
+        #Original version
+        '''S = numpy.divide(numpy.substract(Ni*numpy.sum(numpy.multiply(Y_Bc,Y_Ec),axis=0),
+                                                                 numpy.multiply(Ni*numpy.sum(Y_Bc,axis=0),
+                                                                                Ni*numpy.sum(Y_Ac,axis=0))),
+                                                 numpy.substract(Ni*numpy.sum(numpy.square(Y_Ac),axis=0),
+                                                                 numpy.square(Ni*numpy.sum(Y_Ac,axis=0)))
+                                                 )'''
+        #Simplified indice as samples centered
+        S = numpy.divide(Ni*numpy.sum(numpy.multiply(Y_Bc,Y_Ec),axis=0),
+                                                     Ni*numpy.sum(numpy.square(Y_Ac),axis=0)
+                                                     )
+
+        return S
