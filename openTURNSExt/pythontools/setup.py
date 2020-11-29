40408ce9513587945dfc233c8c46f3d1f21598ed
kramer84 <kristof.attila.private@gmail.com>
2020-11-29 17:54:53 +0100

    updated pythontools setup (no test)

--- openTURNSExt/pythontools/setup.py
+++ openTURNSExt/pythontools/setup.py
@@ -5,13 +5,13 @@ from distutils.version import LooseVersion
 import openturns as ot
 import otmorris as otm
 
-ot_version_require = '1.16rc1'
-if LooseVersion(ot.__version__) != ot_version_require:
-    raise Exception('Version of openturns must be : {}, found {}.'.format(ot_version_require, ot.__version__))
+#ot_version_require = '1.16rc1'
+#if LooseVersion(ot.__version__) != ot_version_require:
+#    raise Exception('Version of openturns must be : {}, found {}.'.format(ot_version_require, ot.__version__))#
 
-otm_version_require = '0.9'
-if LooseVersion(otm.__version__) != otm_version_require:
-    raise Exception('Version of otmorris must be : {}, found {}.'.format(otm_version_require, otm.__version__))
+#otm_version_require = '0.9'
+#if LooseVersion(otm.__version__) != otm_version_require:
+#    raise Exception('Version of otmorris must be : {}, found {}.'.format(otm_version_require, otm.__version__))
 
 # load the version from the file
 with open("VERSION", 'r') as fic:
